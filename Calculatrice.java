16d88eede53adc1d00335ba5db280914f52e4ef7
Sarah H <sarah.heng@umontreal.ca>
2023-03-21 23:09:53 -0400

    add multiplication feature

--- Calculatrice.java
+++ Calculatrice.java
@@ -1,5 +1,5 @@
 public class Calculatrice{
     public static void main(String[] args){
-        System.out.println("Hello Worldeeeeeee!");
+        System.out.println("Hello Worldeeeeeee! Multiplication");
     }
 }
