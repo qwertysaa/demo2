fceac46588c0392c71219b1df37553c379aae7fc
Sarah H <sarah.heng@umontreal.ca>
2023-03-22 00:25:46 -0400

    implement interactive menu display to user

--- Calculatrice.java
+++ Calculatrice.java
@@ -2,5 +2,6 @@ public class Calculatrice{
     public static void main(String[] args){
         System.out.println("Hello Worldeeeeeee! Multiplication pullx2");
         System.out.println("stash test");
+        System.out.println("branch test");
     }
 }
