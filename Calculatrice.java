8bfa9cabbfc702d587cb0e76bb72d8b3ef487b95
Sarah H <sarah.heng@umontreal.ca>
2023-03-21 23:54:48 -0400

    stash test

--- Calculatrice.java
+++ Calculatrice.java
@@ -1,6 +1,6 @@
 public class Calculatrice{
     public static void main(String[] args){
         System.out.println("Hello Worldeeeeeee! Multiplication pullx2");
-        System.out.println("merge test");
+        System.out.println("stash test");
     }
 }
