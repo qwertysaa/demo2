20e1cd41a6d2af26844dd93d4b40ab09e795aef5
Sarah H <sarah.heng@umontreal.ca>
2023-03-21 23:36:18 -0400

    add multiplication feature x2

--- Calculatrice.java
+++ Calculatrice.java
@@ -1,5 +1,6 @@
 public class Calculatrice{
     public static void main(String[] args){
         System.out.println("Hello Worldeeeeeee! Multiplication pullx2");
+        System.out.println("merge test");
     }
 }
